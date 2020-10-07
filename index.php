0fc318cbfab0f4733123ed985c084e0fc85ae089
carlos martinez <cmartinez.meneses1@gmail.com>
2020-10-06 23:33:47 -0500

    cambio

---
+++ index.php
@@ -0,0 +1,489 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+  </head>
+  <body >
+<header class="Header Header-black position-fixed fixed-top">
+  <div class="Header-container d-flex">
+    <div class="Header-logo order-2 order-lg-1">
+      <a href="https://germania.com.pe/">
+        <img src="images/logo-inversiones.png">
+      </a>
+    </div>
+    <div class="Header-menu order-1 order-lg-2">
+      <div class="MenuResponsive">
+        <nav class="MenuResponsive-movil d-lg-none">
+          <button type="button" class="Header-menuButton PanelMenu-button panel-action">
+            <i class="fas fa-bars"></i>
+          </button>
+        </nav>
+        <nav class="d-lg-block nav-uno">
+          <ul>
+            <li><a href="">¿Dudas?</a></li> 
+            <li><a href="">Blogs</a></li> 
+            <li><a href="">Recursos gratuitos</a></li> 
+          </ul>
+        </nav>
+        <nav class="MenuResponsive-desktop  carlos respo d-none d-lg-block">
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <ul>
+            <li class=" "> <a href="">Inicio</a> </li>           
+            <li><a href="">Cursos</a> </li>                         
+            <li><a href="">Aprende de los mejores</a></li> 
+            <li><a href="">Networking</a></li> 
+            <li><a href="">Opiniones</a></li> 
+            <li><a href="">App</a></li>        
+          </ul>
+
+        </nav>
+      </div>
+    </div>
+    <div class="Header-redes order-lg-3 ">
+      <ul>
+        <li><a href=""><img src="images/facebook.png"></a></li>
+        <li><a href=""><img src="images/instagram.png"></a></li>
+        <li><a href=""><img src="images/twitter.png"></a></li>
+        <li><a href=""><img src="images/linkedin.png"></a></li>
+      </ul>
+    </div>
+    <div class="Header-button order-lg-4 ">
+      <button type="button" class="btn button-negro btn-dark">Iniciar Sesión</button>
+    </div>
+  </div>
+</header>
+
+<section class="section-1">
+  <div class="container position-relative">
+  <div class="row">
+    <div class="adornos-1">
+      <img src="images/puntos.png" style="width: 229px;">
+    </div>
+    <div class="col-12 text-center title-banner">
+      <p>LA <span>ESCUELA DE NEGOCIOS </span>QUE <br> ARRASA ENTRE LOS QUE NO <br> BUSCABAN UN MÁSTER</p>
+    </div>
+
+    <div class="col-12 text-center text-online">
+      <p>Online · Clases de 15 min · 499€</p>
+
+      <span>( que puedes pagar a plazos )</span>
+    </div>
+
+      <div class="col-12 text-center my-5">
+        <div  class="buttons">
+          <button type="button" class="btn btn-ver-clases">Quiero ver las clases gratis <img src="images/vector-negro.png"></button>
+          <button type="button" class="btn btn-apuntarme">Apuntarme <img src="images/vector-blanco.png"></button>
+
+          <span>15 días de garantía. SIN PREGUNTAS</span>
+        </div>
+      </div>
+    <div class="adornos-2">
+      <img src="images/adorno-2.png" style="width: 220px;">
+    </div>
+    <div class="col-12 my-5">
+      <div class="row">
+        <div class="col-4 ">
+          <div class="text-center imagen-profesor">
+            <img src="images/jose-llanos.png">
+          </div>
+          <div class="cuadro">
+            <img src="images/lineas-cuadro.png">
+            <p>PROFESOR</p>
+            <span>César Rivero</span>
+          </div>
+        </div>
+        <div class="col-4 ">
+          <div class="text-center imagen-profesor">
+            <img src="images/shopie-lebhard.png">
+          </div>
+          <div class="cuadro">
+            <img src="images/lineas-cuadro.png">
+            <p>PROFESORA</p>
+            <span>Shopie Lebhard</span>
+          </div>
+        </div>
+        <div class="col-4 ">
+          <div class="text-center imagen-profesor">
+            <img src="images/cesar-rivero.png">
+          </div>
+          <div class="cuadro">
+            <img src="images/lineas-cuadro.png">
+            <p>PROFESOR</p>
+            <span>César Rivero</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+</section>
+
+<section class="section-2 mb-5">
+  
+</section>
+
+<section class="section-3">
+  <div class="container">
+    <div class="row"> 
+      <div class="col-12 text-center title-3 mb-5">
+        <p>Másters de Negocio, Marketing Digital y Ecommerce en los que aprendes con los <br> emprendedores y directivos de más éxito en el mundo  <br>
+(fundadores de Tesla,  Shazam, etc.)</p>
+      </div>
+
+      <div class="col-12 mb-4">
+        <div class="row text-center">
+          <div class="col-lg-4">
+            <div class="title-verde"><p>15 min. al día</p></div>
+            <div class="texto-section-3"><p><b>Aprende online desde cualquier  dispositivo</b></p></div>
+          </div>
+          <div class="col-lg-4">
+            <div class="title-verde"><p>Networking Local</p></div>
+            <div class="texto-section-3"><p><b>Comundidades locales en mas de 50  <br>ciudades:</b> Madrid, Valencia, Logroño, <br> Sevilla, Barcelona, Londrés, Lima,  <br>Nueva York, etc.</p></div>
+          </div>
+          <div class="col-lg-4">
+            <div class="title-verde"><p>499€</p></div>
+            <div class="texto-section-3"><p><b>Que además puedes pagar a plazos</b></p></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 text-center mt-5">
+        <div  class="buttons">
+          <button type="button" class="btn btn-ver-clases">Quiero ver las clases gratis <img src="images/vector-negro.png"></button>
+          <button type="button" class="btn btn-apuntarme">Apuntarme <img src="images/vector-blanco.png"></button>
+
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<section class="section-4 my-5">
+  <div class="container"> 
+    <div class="row">
+      <div class="col-7 text-banner2">
+        <p>«Inversores inteligentes está formando a la  <br>próxima generación de  emprendedores y  directivos»</p>
+        <span>- Cesar Rivero, Profesor  </span><br>
+        <img src="images/logos-2.png">
+      </div>      
+    </div>
+  </div>
+</section>
+
+<section class="section-5 mb-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 ajustar">
+        <p class="p-section-5"><b>La mayor comunidad de <br>negocios del mundo</b></p>
+        <p>Más de <b>37.000 alumnos de más de 26 países </b>(España, Colombia, EEUU, Reino Unido, Alemania, etc.)</p>
+      </div>
+      <div class="col-lg-4 ajustar">
+        <p class="p-section-5"><b>Aprende con los mejores</b></p><br>
+        <p>Los <b>co-fundadores</b> de Shazam y Whole Foods, entre otros.</p>
+      </div>
+      <div class="col-lg-4 ajustar">
+        <p class="p-section-5"><b>Más de 400 compañías <br> confían en nosotros</b></p>
+        <p>Google, Coca-Cola, Deloitte, EY, Microsoft y muchos más están <b>formando a sus empleados y buscando candidatos</b> entre nuestros alumnos.</p>
+      </div>
+    </div>    
+  </div>
+</section>
+<section class="section-6 para-que py-5">
+  <div class="container"> 
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>Para qué?</h2>
+      </div>
+      <div class="col-lg-6 pt-4 pb-4">
+        <ul>
+          <li><img src="images/frame.png">Da un <b>salto o giro profesional</b></li>
+          <li><img src="images/frame.png">Lleva tus <b>conocimientos de negocio y </b><br><b class="margen"> Marketing Digital al siguiente nivel</b></li>
+          <li><img src="images/frame.png">Refuerza tu <b>CV y marca personal</b></li>
+        </ul>
+      </div>
+      <div class="col-lg-6 pt-4 pb-4">
+        <ul>
+          <li><img src="images/frame.png"><b>Emprende con garantías de éxito</b></li>
+          <li><img src="images/frame.png">Haz <b>crecer tu negocio/empresa</b></li>
+          <li><img src="images/frame.png"><b>Conecta</b> con miles de <b>personas</b> como tú</li>
+        </ul>
+      </div>
+      <div class="col-12 py-3 title-inversiones text-center">Inversiones inteligentes está ayudando a más de 37.000 personas</div>
+      <div class="col-12">
+        <div class="cuadro-listado row">
+          <div class="col-lg-4">
+            <ul>
+              <li><img src="images/img-listado.png">Emprendedores</li>
+              <li><img src="images/img-listado.png">Ejecutivos</li>
+              <li><img src="images/img-listado.png">Arquitectos</li>
+              <li><img src="images/img-listado.png">Ingenieros</li>
+              <li><img src="images/img-listado.png">Negocio tradicionales</li>
+              <li><img src="images/img-listado.png">Grandes corporaciones</li>
+            </ul>
+          </div>
+          <div class="col-lg-4">
+            <ul>
+              <li><img src="images/img-listado.png">Markets</li>
+              <li><img src="images/img-listado.png">Comerciales</li>
+              <li><img src="images/img-listado.png">Startups</li>
+              <li><img src="images/img-listado.png">Farmacèuticos </li>
+              <li><img src="images/img-listado.png">Freelancers</li>
+              <li><img src="images/img-listado.png">Estudiantes</li>
+            </ul>
+          </div>
+          <div class="col-lg-4">
+            <ul>
+              <li><img src="images/img-listado.png">Empresarios</li>
+              <li><img src="images/img-listado.png">Consultores</li>
+              <li><img src="images/img-listado.png">Agencias</li>
+              <li><img src="images/img-listado.png">Abogados </li>
+              <li><img src="images/img-listado.png">Diseñadores</li>
+              <li><img src="images/img-listado.png">Y mucho màs !!</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+ 
+<section class="section-7 py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2>Cursos</h2>
+      </div>
+    </div>
+
+    <div class="row py-4">
+     
+        <div class="col-lg-4 imagen-fondo">
+          <div class="imagen-oscura"></div>
+         <img src="images/jose-llanos-listado.png" class="img-fluid cuadro-imagen">
+        </div>
+        <div class="col-lg-5 fondo-listado">
+          <h4 class="text-listado1">Real Estate Business</h4>
+          <p class="text-listado2">Profesores . José Llanos </p>
+
+          <span class="text-listado3">Introducción al sector inmobiliario, aprende todo lo necesario a nivel introductor, terminologías, procesos, impuestos, compras y venta de propiedades, intermediación inmobiliaria.</span>
+
+          <p class="text-listado4">España </p>
+          <p class="text-listado5">Latinoamérica</p>
+          <p class="text-listado6">Oferta: 497€.</p>
+
+          <div class="enlace">
+            <a href="" class="rojo">Precio normal: 997€.</a>
+            <a href="" class="negro">Más Información</a>
+          </div>
+        </div>        
+     
+    </div>
+    <div class="row py-4">
+      <div class="col-lg-3"></div>
+        <div class="col-lg-4 imagen-fondo">
+          <div class="imagen-oscura"></div>
+         <img src="images/jose-llanos-listado.png" class="img-fluid cuadro-imagen">
+        </div>
+        <div class="col-lg-5 fondo-listado">
+          <h4 class="text-listado1">Real Estate Business</h4>
+          <p class="text-listado2">Profesores . José Llanos </p>
+
+          <span class="text-listado3">Introducción al sector inmobiliario, aprende todo lo necesario a nivel introductor, terminologías, procesos, impuestos, compras y venta de propiedades, intermediación inmobiliaria.</span>
+
+          <p class="text-listado4">España </p>
+          <p class="text-listado5">Latinoamérica</p>
+          <p class="text-listado6">Oferta: 497€.</p>
+
+          <div class="enlace">
+            <a href="" class="rojo">Precio normal: 997€.</a>
+            <a href="" class="negro">Más Información</a>
+          </div>
+        </div>         
+    </div>
+    <div class="row py-4">
+
+        <div class="col-lg-4 imagen-fondo">
+          <div class="imagen-oscura"></div>
+         <img src="images/jose-llanos-listado.png" class="img-fluid cuadro-imagen">
+        </div>
+        <div class="col-lg-5 fondo-listado">
+          <h4 class="text-listado1">Real Estate Business</h4>
+          <p class="text-listado2">Profesores . José Llanos </p>
+
+          <span class="text-listado3">Introducción al sector inmobiliario, aprende todo lo necesario a nivel introductor, terminologías, procesos, impuestos, compras y venta de propiedades, intermediación inmobiliaria.</span>
+
+          <p class="text-listado4">España </p>
+          <p class="text-listado5">Latinoamérica</p>
+          <p class="text-listado6">Oferta: 497€.</p>
+
+          <div class="enlace">
+            <a href="" class="rojo">Precio normal: 997€.</a>
+            <a href="" class="negro">Más Información</a>
+          </div>
+        </div>  
+      
+    </div>
+     <div class="row py-4">
+      <div class="col-lg-3"></div>
+        <div class="col-lg-4 imagen-fondo">
+          <div class="imagen-oscura"></div>
+         <img src="images/jose-llanos-listado.png" class="img-fluid cuadro-imagen">
+        </div>
+        <div class="col-lg-5 fondo-listado">
+          <h4 class="text-listado1">Real Estate Business</h4>
+          <p class="text-listado2">Profesores . José Llanos </p>
+
+          <span class="text-listado3">Introducción al sector inmobiliario, aprende todo lo necesario a nivel introductor, terminologías, procesos, impuestos, compras y venta de propiedades, intermediación inmobiliaria.</span>
+
+          <p class="text-listado4">España </p>
+          <p class="text-listado5">Latinoamérica</p>
+          <p class="text-listado6">Oferta: 497€.</p>
+
+          <div class="enlace">
+            <a href="" class="rojo">Precio normal: 997€.</a>
+            <a href="" class="negro">Más Información</a>
+          </div>
+        </div>         
+    </div>
+    <div class="row py-4">
+
+        <div class="col-lg-4 imagen-fondo">
+          <div class="imagen-oscura"></div>
+         <img src="images/jose-llanos-listado.png" class="img-fluid cuadro-imagen">
+        </div>
+        <div class="col-lg-5 fondo-listado">
+          <h4 class="text-listado1">Real Estate Business</h4>
+          <p class="text-listado2">Profesores . José Llanos </p>
+
+          <span class="text-listado3">Introducción al sector inmobiliario, aprende todo lo necesario a nivel introductor, terminologías, procesos, impuestos, compras y venta de propiedades, intermediación inmobiliaria.</span>
+
+          <p class="text-listado4">España </p>
+          <p class="text-listado5">Latinoamérica</p>
+          <p class="text-listado6">Oferta: 497€.</p>
+
+          <div class="enlace">
+            <a href="" class="rojo">Precio normal: 997€.</a>
+            <a href="" class="negro">Más Información</a>
+          </div>
+        </div>  
+      
+    </div>
+    <div class="row">
+      <div class="col-12 text-center pt-3 title-inversiones">
+        ¿Tienes dudas? Echa un vistazo a nuestras Clases Gratis
+      </div>
+      <div class="col-12 text-center pt-5">
+        <button class="button-gratis">QUIERO VER LAS CLASES GRATIS</button>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="section-8 py-5" >
+  
+</section>
+
+<!--======================== FOOTER ==============================-->
+<footer class="footer-pagina">
+  <div class="container">
+    <div class="row pt-5">
+      <div class="col-lg-4 list-contacto">
+          <img src="images/logos-2.png" class="img-fluid mb-4">
+
+          <h5>Contacta con nosotros</h5>
+
+        <div class="mb-3 py-4 d-flex"><img src="images/calendario.png"><p style="margin: 0px;padding-left: 10px;">Agenda con un call</p></div>
+
+        <h3>SOPORTE</h3>
+
+        <ul>
+          <li><img src="images/espana.png"><a href="">+34 620 244 218</a></li>
+          <li><img src="images/eeuu.png"><a href="">+1 (756) - 756 1283</a></li>
+        </ul>
+
+        <span>hola@inversoresinteligentes.es</span>
+        <br>
+        <br>
+        <br>
+
+        <h3>PARA EMPRESAS:</h3>
+
+        <span>empresas@inversoresinteligentes.es</span>
+
+        <ul>
+          <li><img src="images/whatsApp.png" style="width: 24px !important;"><a href="">WhatsApp</a></li>
+        </ul>
+
+        <br>
+        <h3>PRENSA</h3>
+
+        <span>prensa@inversores inteligente.com</span>
+      </div>
+      <div class="col-lg-2 p-0">
+        <h5>Contenido</h5>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Aprende de los mejores</a></li>
+          <li><a href="">Networking</a></li>
+          <li><a href="">Opiniones</a></li>
+          <li><a href="">Manifiesto</a></li>
+          <li><a href="">Dudas</a></li>
+          <li><a href="">Blog</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-4 pl-5">
+        <h5>Másters</h5>
+        <ul>
+          <li><a href="">Máster Inversores Inteligentes</a></li>
+          <li><a href="">Máster</a></li>
+          <li><a href="">Inversores DigitalMarketing</a></li>
+          <li><a href="">Máster Inversores Inteligentes</a></li>
+          <li><a href="">Máster Inversores Inteligentes</a></li>
+          <li><a href="">Máster Inversores Inteligentes</a></li>
+          <li><a href="">FutureLeaders</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-2 p-0">
+        <h5>Legal</h5>
+        <ul>
+          <li><a href="">Términos y condiciones</a></li>
+          <li><a href="">Política de privacidad</a></li>
+          <li><a href="">Política de cookies</a></li>
+          <li><a href="">Aviso legal</a></li>
+        </ul>
+      </div>
+      <div class="redes-footer ">
+        <ul>
+          <li><a href=""><img src="images/redes-1.png"></a></li>
+          <li><a href=""><img src="images/redes-2.png"></a></li>
+          <li><a href=""><img src="images/redes-3.png"></a></li>
+        </ul>
+      </div>
+      <div class="col-12 text-center pt-5 pb-4 copyright">
+        <span>Copyright © 2020 | All Rights Reserved</span>
+      </div>
+      
+    </div>
+  </div>
+</footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
